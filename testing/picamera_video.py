6b451e51b2e4d9a4e4969b0ae6820e778a618d2c
jshet <jshet@github.com>
2022-10-15 15:07:30 -0500

    rename and stuff

--- testing/picamera_video.py
+++ testing/picamera_video.py
@@ -8,7 +8,7 @@ time.sleep(2)
 camera.resolution = (1920, 1080)
 
 timestamp = datetime.now().isoformat()
-save_as = "/home/pi/" + timestamp + "h.264"
+save_as = "" + timestamp + ".h264"
 
 camera.start_recording(save_as)
 camera.wait_recording(8)
--- testing/steppers.py
+++ testing/steppers.py
@@ -61,8 +61,8 @@ def move_back(motor, step_list=steps, this_many=three_sixty, this_much=delay):
     
 
 
-move(motor1, this_many=400, this_much=1100)
-move_back(motor1, this_many=800, this_much=1100)
+#move(motor1, this_many=1200, this_much=1100)
+move_back(motor1, this_many=1200, this_much=1100)
 #move(motor1, this_many=200, this_much=1200)
 #move(motor1, this_many=200, this_much=1000)
 #move(motor1, this_many=200, this_much=800)
