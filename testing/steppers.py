46298b70833d75860e841814682378c406171976
jshet <jshet@github.com>
2022-11-28 14:33:38 -0600

    refactoring for command line

--- testing/steppers.py
+++ testing/steppers.py
@@ -8,28 +8,20 @@ m4 = Pin(5, Pin.OUT)
 
 motor1 = [m1, m2, m3, m4]
 
-three_sixty = 200
-how_far = three_sixty
+motor_step_angle = 1.8
+step_mode = "half"
+step_angle = motor_step_angle / 2 if step_mode == "half" else motor_step_angle
+
+tpi = 12
+tpmm = tpi / 25.4
+
+steps_per_inch = tpi * (step_angle * 360)
+steps_per_mm = tpmm * (step_angle * 360)
 
 delay = 1000
 
-full_steps = [
-    [1,1,0,0],
-    [0,1,1,0],
-    [0,0,1,1],
-    [1,0,0,1]
-    ]
-
-steps = [
-    [1,0,0,0],
-    [1,1,0,0],
-    [0,1,0,0],
-    [0,1,1,0],
-    [0,0,1,0],
-    [0,0,1,1],
-    [0,0,0,1],
-    [1,0,0,1]
-   ]
+full_steps = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
+steps = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
 
 def send_it(motor, a, b, c, d):
     motor[0].value(a)
@@ -41,27 +33,61 @@ def end_it():
     for x in motor1:
         x.value(0)
 
-def move(motor, step_list=steps, this_many=three_sixty, this_much=delay):
+def move_cw(motor, step_list=steps, this_many=1, this_much=delay):
     for i in range(this_many):
         print(i)
         for move in step_list:
             send_it(motor, move[0],move[1],move[2],move[3])
             time.sleep_us(this_much)
     send_it(motor, 0,0,0,0)
-    time.sleep(0.25)
+    time.sleep(0.1)
 
-def move_back(motor, step_list=steps, this_many=three_sixty, this_much=delay):
+def move_ccw(motor, step_list=steps, this_many=1, this_much=delay):
     step_list.reverse()
-    for i in range(this_many):
+    print("moving ccw...")
+    for i in range(this_many*-1):
         print(i)
         for move in step_list:
             send_it(motor, move[0],move[1],move[2],move[3])
             time.sleep_us(this_much)
     send_it(motor, 0,0,0,0)
-    time.sleep(0.25)
+    time.sleep(0.1)
     step_list.reverse()
 
-move(motor1, this_many=200, this_much=1500)
-move_back(motor1, step_list=full_steps, this_many=200, this_much=3000)
+def distance_to_steps(x):
+    try:
+        return float(x)
+    except:
+        x, unit = (float(x[:-2]), x[-2:].lower())
+        if "in" in unit:
+            steps = x * steps_per_inch
+        elif "mm" in unit:
+            steps = x * steps_per_mm
+        elif "cm" in unit:
+            steps = x * steps_per_mm / 10
+        else:
+            print("Unit of measure not recognized. Try 'in', 'inch', 'cm', 'mm' or leave blank for steps.")
+        return steps
+    
+def move(x, motor=motor1, direction=1, speed=2500):
+    print(f"Preparing to move {x=}, {motor=}, {direction=}")
+    if direction == 1:
+        move_cw(motor, this_many=steps, this_much=speed)
+    elif direction == -1:
+        move_ccw(motor, this_many=steps, this_much=speed)
+    else:
+        print("Need to ask for directions?")
+
+while True:
+    distance = input("slider>>>")
+    steps = distance_to_steps(distance)
+    direction = 1 if steps > 0 else -1
+    move(steps, direction=direction, speed=1500)
+    print(f"{steps=}, {direction=}")
+
+#move(motor1, this_many=100, this_much=2500)
+#move_back(motor1, this_many=400, this_much=2500)
+#move_back(motor1, this_many=44, this_much=2500)
+#move_back(motor1, step_list=full_steps, this_many=200, this_much=3000)
 
 end_it()
